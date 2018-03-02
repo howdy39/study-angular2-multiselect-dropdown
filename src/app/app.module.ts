2c75281a308cc221a6f4c6487196766c4ca4994e
howdy39 <tatsuya.nakano@topgate.co.jp>
2018-03-02 16:31:20 +0900

    腹持ち固定表示のマルチセレクトを追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { Case1Component } from './case1/case1.component';
+import { AngularMultiSelectModule } from 'angular2-multiselect-dropdown';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Case1Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularMultiSelectModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
