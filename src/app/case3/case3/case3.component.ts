803c3784cc4adc5787f4329dd6b409ec866fd160
howdy39 <tatsuya.nakano@topgate.co.jp>
2018-03-06 15:01:47 +0900

    ドロップダウンを追加

--- src/app/case3/case3/case3.component.ts
+++ src/app/case3/case3/case3.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ItemService } from '../../item.service';
 
 @Component({
   selector: 'app-case3',
@@ -6,10 +7,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./case3.component.css']
 })
 export class Case3Component implements OnInit {
+  dropdownList = [];
+  selectedItems = [];
+  dropdownSettings = {};
 
-  constructor() { }
+  constructor(
+    private itemService: ItemService
+  ) { }
 
   ngOnInit() {
+    console.log('AAA');
+    setTimeout(() => {
+      console.log('BBB')
+      this.itemService.getItems().subscribe(items => this.dropdownList = items);
+    }, 3000);
+
+    this.selectedItems = [
+      {'id': 2, 'itemName': 'Singapore'},
+      {'id': 3, 'itemName': 'Australia'},
+      {'id': 4, 'itemName': 'Canada'},
+      {'id': 5, 'itemName': 'South Korea'}
+    ];
+    this.dropdownSettings = {
+      singleSelection: false,
+      text: 'Select Countries',
+      selectAllText: 'All',
+      unSelectAllText: 'All',
+    };
+
+  }
+
+  onItemSelect(item: any) {
+    console.log(item);
+    console.log(this.selectedItems);
+  }
+  onItemDeSelect(item: any) {
+    console.log(item);
+    console.log(this.selectedItems);
+  }
+  onSelectAll(items: any) {
+    console.log(items);
+  }
+  onDeSelectAll(items: any) {
+    console.log(items);
   }
 
 }
